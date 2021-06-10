e71f8279c4b56b6821487d80f4eeed4e29749e9e
ThanhCong99ND <59520616+ThanhCong99ND@users.noreply.github.com>
2021-06-10 16:11:07 +0700

    login

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import LoginForm from './src/components/LoginForm';
+import LoginForm from './src/components/LoginForm.js';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => LoginForm);
