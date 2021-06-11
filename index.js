0bee7b32d47a69faea42bec49d446ecb7164e186
lediep2809 <35600474+lediep2809@users.noreply.github.com>
2021-06-11 18:39:40 +0700

    add menu

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ import {AppRegistry} from 'react-native';
 import LoginForm from './src/components/LoginForm.js';
 import Registration from './src/components/Registration.js';
 import WellCome from './src/components/WellCome.js';
+import menu from './src/Navigator/Menus.js';
 import {name as appName} from './app.json';
 
 
-AppRegistry.registerComponent(appName, () => WellCome);
+AppRegistry.registerComponent(appName, () => menu);
 
 
 
