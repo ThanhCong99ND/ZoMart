fc1cc23e4aa7765b288c1f3d286a9f8a85dfbbf2
ThanhCong99ND <59520616+ThanhCong99ND@users.noreply.github.com>
2021-06-13 00:10:33 +0700

    abc

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-import {AppRegistry} from 'react-native';
+import {AppRegistry} from "react-native";
 import LoginForm from './src/components/LoginForm.js';
 import Registration from './src/components/Registration.js';
 import WellCome from './src/components/WellCome.js';
 import WellCometoLogin from './src/components/WellCometoLogin.js';
-
 import {name as appName} from './app.json';
+
+
+
 
 AppRegistry.registerComponent(appName, () => WellCometoLogin);
 
