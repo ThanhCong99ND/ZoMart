4479609779f4c4a92ac84e8a9dd59688bc70bd3e
ThanhCong99ND <59520616+ThanhCong99ND@users.noreply.github.com>
2021-06-12 16:30:20 +0700

    logintores

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
 import LoginForm from './src/components/LoginForm.js';
 import Registration from './src/components/Registration.js';
 import WellCome from './src/components/WellCome.js';
+import WellCometoLogin from './src/components/WellCometoLogin.js';
 import {name as appName} from './app.json';
-
-
-AppRegistry.registerComponent(appName, () => WellCome);
-
-
-
+AppRegistry.registerComponent(appName, () => WellCometoLogin);
--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,65 +1,80 @@
-import React, { Component } from 'react'
-import { Platform, StyleSheet, Text, View, Image, TextInput, Button, TouchableOpacity, StatusBar } from "react-native";
-import Ionicons from 'react-native-vector-icons/Ionicons'
-import FontAwesome from 'react-native-vector-icons/FontAwesome'
+import React, {Component} from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Keyboard,
+  StatusBar,
+} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
-export default class LoginForm extends Component {
+export default class Registration extends Component {
   render() {
     return (
-      <View style={styles.container}>
-
-        <Image style={styles.image} source={require('../assets/Splash.png')} />
-        
-        <StatusBar style="auto" />
-        <Text style={styles.Text}>Đăng Ký</Text>
-        <View style={styles.name}>
-          <TextInput
-            style={styles.TextInput}
-            textContentType='name'
-            keyboardType='default'
-            placeholder=" Họ và Tên"
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.container}>
+          <Image
+            style={styles.image}
+            source={require('../assets/Splash.png')}
           />
-        </View>
-        <View style={styles.EmailView}>
-          <TextInput
-            style={styles.TextInput}
-            textContentType='emailAddress'
-            keyboardType='number-pad'
-            placeholder=" Số điện thoại"
-          />
-        </View>
 
-        <View style={styles.EmailView}>
-          <TextInput
-            style={styles.TextInput}
-            textContentType='emailAddress'
-            keyboardType='email-address'
-            placeholder=" Email"
-          />
-        </View>
+          <StatusBar style="auto" />
+          <View style={styles.name}>
+            <TextInput
+              style={styles.TextInput}
+              textContentType="name"
+              keyboardType="default"
+              placeholder=" Họ và Tên"
+            />
+          </View>
+          <View style={styles.EmailView}>
+            <TextInput
+              style={styles.TextInput}
+              textContentType="emailAddress"
+              keyboardType="number-pad"
+              placeholder=" Số điện thoại"
+            />
+          </View>
 
-        <View style={styles.PasswordView}>
-          <TextInput
-            style={styles.TextInput}
-            placeholder="Mật khẩu"
-            secureTextEntry={true}
-            // onChangeText={(password) => setPassword(password)}
-          />
-        </View>
+          <View style={styles.EmailView}>
+            <TextInput
+              style={styles.TextInput}
+              textContentType="emailAddress"
+              keyboardType="email-address"
+              placeholder=" Email"
+            />
+          </View>
 
-        <View style={styles.PasswordView}>
-          <TextInput
-            style={styles.TextInput}
-            placeholder="Nhập lại mật khẩu"
-            secureTextEntry={true}
-          //   onChangeText={(password) => setPassword(password)}
-          />
-        </View>
+          <View style={styles.PasswordView}>
+            <TextInput
+              style={styles.TextInput}
+              placeholder="Mật khẩu"
+              secureTextEntry={true}
+              // onChangeText={(password) => setPassword(password)}
+            />
+          </View>
 
-        <TouchableOpacity style={styles.RegiBtn}>
-          <Text style={styles.ReText}>Đăng ký</Text>
-        </TouchableOpacity>
-      </View>
+          <View style={styles.PasswordView}>
+            <TextInput
+              style={styles.TextInput}
+              placeholder="Nhập lại mật khẩu"
+              secureTextEntry={true}
+              //   onChangeText={(password) => setPassword(password)}
+            />
+          </View>
+
+          <TouchableOpacity style={styles.RegiBtn}>
+            <Text style={styles.ReText}>Đăng ký</Text>
+          </TouchableOpacity>
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
@@ -67,42 +82,41 @@ export default class LoginForm extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#FFB039",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#FFB039',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 
   image: {
     width: 190,
     height: 180,
-
   },
-  name:{
-    backgroundColor: "#FFFFFF",
+  name: {
+    backgroundColor: '#FFFFFF',
     borderRadius: 30,
-    width: 377,
+    width: 300,
     height: 54,
     marginTop: 2,
-    alignItems: "center",
+    alignItems: 'center',
     marginBottom: 15,
   },
 
   EmailView: {
-    backgroundColor: "#FFFFFF",
+    backgroundColor: '#FFFFFF',
     borderRadius: 30,
-    width: 377,
+    width: 300,
     height: 54,
     marginTop: 2,
-    alignItems: "center",
+    alignItems: 'center',
     marginBottom: 15,
   },
   PasswordView: {
-    backgroundColor: "#FFFFFF",
+    backgroundColor: '#FFFFFF',
     borderRadius: 30,
-    width: 377,
+    width: 300,
     height: 54,
     marginTop: 2,
-    alignItems: "center",
+    alignItems: 'center',
     marginBottom: 15,
   },
 
@@ -111,46 +125,40 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     marginLeft: 0,
-
   },
 
   RegiBtn: {
-    width: "70%",
+    width: '70%',
     borderRadius: 25,
     height: 52,
-    alignItems: "center",
-    justifyContent: "center",
-    textShadowColor:"#5D657B",
-    backgroundColor: "#5D657B",
+    alignItems: 'center',
+    justifyContent: 'center',
+    textShadowColor: '#5D657B',
+    backgroundColor: '#5D657B',
     marginBottom: 25,
     borderWidth: 2,
     borderColor: '#000',
   },
 
-  ReText:{
-    color: "#FFFCFC",
+  ReText: {
+    color: '#FFFCFC',
   },
 
-  Text:{
-    color: "#000000",
-    fontSize: 25 ,
+  Text: {
+    color: '#000000',
+    fontSize: 25,
     marginBottom: 20,
-    fontWeight: "bold"
-    
+    fontWeight: 'bold',
   },
 
-  facebookButton:{
-    width: "80%",
+  facebookButton: {
+    width: '80%',
     height: 50,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
     marginBottom: 10,
-
   },
-  facebooktext:{
+  facebooktext: {
     marginLeft: 0,
-  }
-  
+  },
 });
-
-
